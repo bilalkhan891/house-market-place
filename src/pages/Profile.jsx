48a2d0857da1a1afc3328e383c43a477402a6139
Bilal Ahmad Afridi <bilalkhan891@hotmail.com>
2022-10-24 01:45:30 +0500

    user sign-in and displaying user display name on profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { getAuth } from "firebase/auth";
 
 function Profile() {
+  const [user, setUser] = useState(null);
+  const auth = getAuth();
+
+  useEffect(() => {
+    console.log(auth.currentUser);
+    setUser(auth.currentUser);
+  }, []);
+
   return (
     <div>
       <h1>Profile</h1>
+      {user && <p>{user.displayName}</p>}
     </div>
   );
 }
