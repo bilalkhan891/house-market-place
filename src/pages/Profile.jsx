e50e4179225e4b314b0cd640136573f83d2e5770
Bilal Ahmad Afridi <bilalkhan891@hotmail.com>
2022-10-28 15:41:15 +0500

    Logout button added to profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,19 +1,32 @@
 import { useEffect, useState } from "react";
 import { getAuth } from "firebase/auth";
+import { useNavigate, Link } from "react-router-dom";
 
 function Profile() {
-  const [user, setUser] = useState(null);
   const auth = getAuth();
+  console.log(auth)
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email
+  });
+   
+  const {name, email } = formData
+  const navigate = useNavigate()
 
-  useEffect(() => {
-    console.log(auth.currentUser);
-    setUser(auth.currentUser);
-  }, []);
+  const onLogout = () => {
+    auth.signOut()
+    navigate("/")
+  }
 
   return (
-    <div>
-      <h1>Profile</h1>
-      {user && <p>{user.displayName}</p>}
+    <div className="profile">
+      <header className="profileHeader">
+        <h1 className="pageHeader">Profile</h1>
+      </header>
+      <h2>{name && name}</h2>
+      <button type="button" className="logOut" onClick={onLogout}>
+        Logout
+      </button>
     </div>
   );
 }
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -125,4 +125,4 @@ function Signup() {
   );
 }
 
-export default Signup;
+export default Signup; 
